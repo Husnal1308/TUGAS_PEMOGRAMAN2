f9bdbae5347bc088d95d0c1f5f7407b71625f54f
Husnal1308 <alwa09254@gmail.com>
2025-12-02 19:08:39 +0700

    Delete pertemuan5_S3_OVERRIDING directory

--- pertemuan5_S3_OVERRIDING/Kucing.java
+++
@@ -1,8 +0,0 @@
-package pertemuan5_S3_OVERRIDING;
-
-public class Kucing {
-    // Method yang akan di-override
-    public void suara() {
-        System.out.println("Kucing bersuara meong...");
-    }
-}
